672a9a00e234f3dd579ded2cb159823d0e028c8e
Mario Camelo <mariuo@gmail.com>
2022-05-20 09:17:28 -0400

    project create

---
+++ src/main/java/com/mcamelo/imdbDefi/ImdbDefiApplication.java
@@ -0,0 +1,13 @@
+package com.mcamelo.imdbDefi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbDefiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbDefiApplication.class, args);
+	}
+
+}
